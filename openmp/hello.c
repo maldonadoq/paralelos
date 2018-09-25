179ae48af22381a35dd46a8fd2e7146e3bef48e7
Percy Maldonado Quispe <percy.maldonado@ucsp.edu.pe>
2018-09-25 09:19:01 -0500

    openmp code

---
+++ openmp/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+void hello(){
+	int my_rank = omp_get_thread_num();
+	int thc = omp_get_num_threads();
+	printf("Hello from thread %d of %d\n", my_rank, thc);
+}
+
+int main(int argc, char const *argv[]){
+	int thc = 4;
+	
+	#pragma omp parallel num_threads(thc)
+		hello();
+	return 0;
+}
